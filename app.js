cdda8c3e579a9cda8eb3c9ea6d0139f6fef1f3ab
Ishanax <67413079+Ishanax@users.noreply.github.com>
2021-01-25 16:25:00 +0100

    finished

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var quotes = [
-  "The difficulty in philosophy is to say no more than we know. - 1935",
-  "What cannot be imagined cannot even be talked about. - 1916",
-  "Uttering a word is like striking a note on the keyboard of the imagination. - 1953",
-  "The world is the totality of facts, not things. -1922",
-  "I work quite diligently and wish that I were better and smarter. And these both are one and the same. - 1917",
-  "One often makes a remark and only later sees how true it is. - 1914",
-  "Logic takes care of itself; all we have to do is to look and see how it does it. - 1914",
-  "My difficulty is only an - enormous- difficulty of expression. - 1915"
+  "The difficulty in philosophy is to say no more than we know.",
+  "What cannot be imagined cannot even be talked about.",
+  "Uttering a word is like striking a note on the keyboard of the imagination.",
+  "The world is the totality of facts, not things.",
+  "I work quite diligently and wish that I were better and smarter. And these both are one and the same.",
+  "One often makes a remark and only later sees how true it is.",
+  "Logic takes care of itself; all we have to do is to look and see how it does it.",
+  "If people never did silly things nothing intelligent would ever get done.",
+  "A serious and good philosophical work could be written consisting entirely of jokes.",
+  "Nothing is so difficult as not deceiving oneself.",
+  "My difficulty is only an - enormous- difficulty of expression."
 ]
 
 function newQuote(){ 
